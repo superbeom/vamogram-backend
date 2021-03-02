822ed5753a3219785dd037083e890f855aa9b338
superbeom <it.super.beom@gmail.com>
2021-03-02 16:07:36 +0900

    #3.11 Dotenv

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 
@@ -5,6 +6,10 @@ const server = new ApolloServer({
   schema,
 });
 
+const port = process.env.PORT;
+
 server
-  .listen()
-  .then(() => console.log(`Server is running on http://localhost:4000/`));
+  .listen(port)
+  .then(() =>
+    console.log(`🚀 Server is running on http://localhost:${port} ✅`)
+  );
