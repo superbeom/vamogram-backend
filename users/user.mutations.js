be50264a785204fd0bda0e2aaef4c1badfa455d1
superbeom <it.super.beom@gmail.com>
2021-03-04 12:08:59 +0900

    #4.1 Create Account part Two

--- users/user.mutations.js
+++ users/user.mutations.js
@@ -1,3 +1,20 @@
+import client from "../client";
+
 export default {
-  Mutation: {},
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      // Check if username or email are already on DB
+      const existingUser = await client.user.findFirst({
+        where: {
+          OR: [{ username }, { email }],
+        },
+      });
+
+      // Hash password
+      // Save and return the user
+    },
+  },
 };
