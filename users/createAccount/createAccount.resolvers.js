c228467d56a0bb6c290d189d95b936daead177a5
superbeom <it.super.beom@gmail.com>
2021-03-04 16:37:25 +0900

    #4.6 Divide and Conquer

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,6 +1,5 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import client from "../client";
+import client from "../../client";
 
 export default {
   Mutation: {
@@ -36,38 +35,6 @@ export default {
       } catch (error) {
         return error;
       }
-    },
-
-    login: async (_, { username, password }) => {
-      // Find user with args,username
-      const user = await client.user.findUnique({
-        where: {
-          username,
-        },
-      });
-      if (!user) {
-        return {
-          ok: false,
-          error: "User not found.",
-        };
-      }
-
-      const passwordOk = await bcrypt.compare(password, user.password);
-      if (!passwordOk) {
-        return {
-          ok: false,
-          error: "Incorrect password.",
-        };
-      }
-
-      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
-      return {
-        ok: true,
-        token,
-      };
-
-      // Check password with args.password
-      // Issue a token and send it to the user
     },
   },
 };
