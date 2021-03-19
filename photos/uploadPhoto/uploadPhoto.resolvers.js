859dd00873d2f40a811875255267407c44087b46
superbeom <it.super.beom@gmail.com>
2021-03-19 13:41:33 +0900

    #6.3 Upload Photo part Two

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,9 +1,30 @@
+import client from "../../client";
 import { protectedResolver } from "../../users/users.utils";
 
 const resolverFn = async (_, { file, caption }, { loggedInUser }) => {
   if (caption) {
     // parse caption
+    const hashtags = caption.match(/#[\w]+/g);
+
     // get or create Hashtags if hashtags in caption
+    await client.photo.create({
+      data: {
+        file,
+        caption,
+        hashtags: {
+          connectOrCreate: [
+            {
+              where: {
+                hashtag: "#food",
+              },
+              create: {
+                hashtag: "#food",
+              },
+            },
+          ],
+        },
+      },
+    });
   }
   // save the photo WITH the parsed hashtags
   // add the photo to the Hashtags
