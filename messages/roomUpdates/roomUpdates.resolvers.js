62e15a9b2711a810b44e5feac5f6ec3dedb84ab4
superbeom <it.super.beom@gmail.com>
2021-03-22 10:14:30 +0900

    #7.10 Filtering Subscriptions part Two

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -1,14 +1,30 @@
 import pubsub from "../../pubsub";
 import { NEW_MESSAGE } from "../../constants";
 import { withFilter } from "apollo-server-express";
+import client from "../../client";
 
 export default {
   Subscription: {
     roomUpdates: {
-      subscribe: withFilter(
-        () => pubsub.asyncIterator(NEW_MESSAGE),
-        ({ roomUpdates: { roomId } }, { id }) => roomId === id
-      ),
+      subscribe: async (_, { id }) => {
+        const room = await client.room.findUnique({
+          where: {
+            id,
+          },
+          select: {
+            id: true,
+          },
+        });
+
+        if (!room) {
+          throw new Error("Room not found");
+        }
+
+        return withFilter(
+          () => pubsub.asyncIterator(NEW_MESSAGE),
+          ({ roomUpdates: { roomId } }, { id }) => roomId === id
+        )(_, { id });
+      },
     },
   },
 };
